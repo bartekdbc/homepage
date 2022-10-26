2861dcf3e1d1d5c1c30e0bbf6c49271380bd0813
Bartek Dębiec <bartekdbc@gmail.com>
2022-10-26 12:17:23 +0200

    functions JS updated

--- js/script.js
+++ js/script.js
@@ -1,23 +1,42 @@
-console.log("Hej, miłego dnia dla wszystkich, którzy tu zajrzą!");
+{
+  const welcome = () => {
+    console.log("Hej, miłego dnia dla wszystkich, którzy tu zajrzą!");
+  };
 
-let header__buttonElement = document.querySelector(".js-button");
-let bodyElement = document.querySelector(".js-body");
-let header__themeNameElement = document.querySelector(".js-themeName");
-let section__buttonContactElement = document.querySelector(".js-buttonContact");
-let section__contactElement = document.querySelector(".js-contact");
+  const toggleBackground = () => {
+    const bodyElement = document.querySelector(".js-body");
+    const header__themeNameElement = document.querySelector(".js-themeName");
 
-header__buttonElement.addEventListener("click", () => {
-  bodyElement.classList.toggle("body--darkTheme");
+    bodyElement.classList.toggle("body--darkTheme");
+    header__themeNameElement.innerText = bodyElement.classList.contains(
+      "body--darkTheme"
+    )
+      ? "Jasny"
+      : "Ciemny";
+  };
 
-  header__themeNameElement.innerText = bodyElement.classList.contains("body--darkTheme")
-    ? "Jasny"
-    : "Ciemny";
-});
+  const showDetails = () => {
+    const section__contactElement = document.querySelector(".js-contact");
+    const section__buttonContactElement =
+      document.querySelector(".js-buttonContact");
 
-section__buttonContactElement.addEventListener("click", () => {
-  section__contactElement.classList.toggle("section__contact--hide");
-  section__buttonContactElement.innerText =
-    section__contactElement.classList.contains("section__contact--hide")
-      ? "Pokaż kontakt"
-      : "Ukryj kontakt";
-});
+    section__contactElement.classList.toggle("section__contact--hide");
+    section__buttonContactElement.innerText =
+      section__contactElement.classList.contains("section__contact--hide")
+        ? "Pokaż kontakt"
+        : "Ukryj kontakt";
+  };
+
+  const init = () => {
+    const header__buttonElement = document.querySelector(".js-button");
+    const section__buttonContactElement =
+      document.querySelector(".js-buttonContact");
+
+    header__buttonElement.addEventListener("click", toggleBackground);
+    section__buttonContactElement.addEventListener("click", showDetails);
+
+    welcome();
+  };
+
+  init();
+}
